240f61e19adfd634945eeac45a2e8429c3a690e7
Amanda_tian <amanda_tian@100-64-33-148.wireless.ucsb.edu>
2026-04-20 14:55:24 -0700

    set python version for deployment

--- concert_plan_recommender/settings.py
+++ concert_plan_recommender/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = 'static/'
 
+STATIC_DIRS = [
+    BASE_DIR / 'recommend_plan/static',
+]
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'weights'
 LOGOUT_REDIRECT_URL= 'weights'
